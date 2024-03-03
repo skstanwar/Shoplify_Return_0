83ed15dff4c4b2d6fbd04a6e3dfefda1e7841960
skstanwar <kumarsaini0098@gmail.com>
2024-03-03 23:45:42 +0530

    fix post upload

--- server/app.js
+++ server/app.js
@@ -28,6 +28,7 @@ const views= join(filePath,'views');
 app.set('view engine', 'ejs');	
 app.set('views', views);
 app.use(express.static(staticPath));
+app.use(express.static(publicPath));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
